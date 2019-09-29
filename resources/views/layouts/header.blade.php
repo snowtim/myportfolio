f7588dfe55009c51df47d8ddeade070514ebdb4b
Your Name <you@example.com>
2019-09-30 00:00:08 +0800

    add icon of cart

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,6 @@
 <h1 class="site-heading text-center text-white d-none d-lg-block">
 	<div id="cart" class="site-heading-upper text-right" style="font-size:medium">
+		<i class="fas fa-shopping-cart"></i>
 		<a href="/cart">Your Cart</a>
 	</div>
     <span class="site-heading-upper text-primary mb-3">A Free Bootstrap 4 Business Theme</span>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,12 +13,15 @@
   <!-- Bootstrap core CSS -->
   <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
   <!-- Custom fonts for this template -->
   <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="{{ asset('css/business-casual.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('css/business-casual.css') }}" rel="stylesheet">
 
 </head>
 
